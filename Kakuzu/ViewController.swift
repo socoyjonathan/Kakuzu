06d1eeb800366926a64f4b674423fd87bb1afd03
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2023-06-05 08:57:56 -0400

    template for rest of app

--- Kakuzu/ViewController.swift
+++ Kakuzu/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
              [0,0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0,0]]
     
+    var num = 0
+    
+    var turn = 0
+    
     // players
     var player1 = "Player 1"
     var player2 = "Player 2"
@@ -27,51 +31,78 @@ class ViewController: UIViewController {
     // players scores
     var player1_score = 0
     var player2_score = 0
-    
-    // tile 1 test
-    var randomGuess = 0
-    var firstTile: String = "1"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(board)
-        populateBoard()
-        print(board)
-        fillBoard()
+        resetBoard()
         // Do any additional setup after loading the view.
     }
 
+    
+    func resetBoard() {
+        
+        board = [[0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0]]
+        
+        populateBoard()
+        fillBoard()
+    }
+    
+    
+    @IBAction func resetClicked(_ sender: UIButton) {
+        
+        //cover all tiles
+        
+        resetBoard()
+        
+    }
+    
+    @IBAction func newNumberClicked(_ sender: UIButton) {
+        
+        
+        num = Int.random(in: 1..<10)
+        
+        //show this to the user
+        
+    }
     
     // buttons actions
     //https://stackoverflow.com/questions/38534573/changing-opacity-of-button-when-clicked-xcode-swift
     @IBAction func buttonClicked(_ sender: UIButton) {
         
-        // set background color to white
-        //https://stackoverflow.com/questions/24427284/change-button-background-color-using-swift-language
         sender.backgroundColor = UIColor.white
+   
         
-        // dynamically set the button text to the number in the array at that position
-        //https://stackoverflow.com/questions/26326296/changing-text-of-uibutton-programmatically-swift
-        //sender.setTitle(firstTile, for: UIControl.State.normal)
-        
-        // select a random number that the user needs to guess correct square
-        //https://stackoverflow.com/questions/24007129/how-to-generate-a-random-number-in-swift
-        // TODO: implement a separate method and button to display number
-        randomGuess = Int.random(in: 0..<9)
-        
-        // if user guessed correctly, remove cover and display number
-        // TODO: add points to each player who guesses correctly
-        if randomGuess == 0 {
+        //check if guess was correct
+        if Int(String(sender.currentTitle!)) == num {
             sender.backgroundColor = UIColor.white
-        } else {
+            
+            //check to see if that number is done
+        }
+        
+        //if not correct, cover tile and change turns
+        else {
             // delay by 1 second, then cover number if guess incorrect
             DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
                 sender.backgroundColor = UIColor.black
                 //sender.setTitle("", for: UIControl.State.normal)
+                self.turn = (self.turn + 1) % 2
+                
+                //show turn change to user
             }
         }
     }
+
     
+    //fill the buttons with the values from the "board" global variable
     func fillBoard() {
         
         var list = 0
@@ -96,6 +127,7 @@ class ViewController: UIViewController {
          
     }
     
+    //helper function for populateBoard
     func in_square(x : Int, y : Int, val : Int) -> Bool {
         
         let r = Int(3 * floor(Float(x) / 3))
@@ -113,6 +145,7 @@ class ViewController: UIViewController {
         
     }
     
+    //generate a new sudoku board in the "board" global variable
     func populateBoard() {
         
         var count = 0
@@ -122,7 +155,7 @@ class ViewController: UIViewController {
         var redo = false
         
         while (r < 9) {
-            print("row: " + String(r))
+            //print("row: " + String(r))
             
             var numbers = [1,2,3,4,5,6,7,8,9]
             var c = 0
@@ -138,20 +171,15 @@ class ViewController: UIViewController {
             
             
             while (!redo && c < 9) {
-                print("col: " + String(c))
+               // print("col: " + String(c))
                 
                 
 
                 while (!redo && count < 20) {
-                    if (c == 8) {
-                        print(count)
-                    }
                     
                     
                     let val = numbers.randomElement()!
-                    if (count == 0) {
-                        print(val)
-                    }
+
                     let col = [board[0][c],board[1][c],board[2][c],
                                board[3][c],board[4][c],board[5][c],
                                board[6][c],board[7][c],board[8][c]]
@@ -174,7 +202,6 @@ class ViewController: UIViewController {
                 
                 if (count >= 20) {
                     count2 = count2 + 1
-                    print("redo")
                     board[r] = [0,0,0,0,0,0,0,0,0]
                     r = r - 1
                     count = 0
